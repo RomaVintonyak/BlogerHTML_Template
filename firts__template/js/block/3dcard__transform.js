c3e7a30bc2efa61d376c64ec31f3a0ee14ae25e2
RomaVintonyak <19roman23@gmail.com>
2020-03-20 16:32:21 +0200

    fixed bag to 3d translate card

--- firts__template/js/block/3dcard__transform.js
+++ firts__template/js/block/3dcard__transform.js
@@ -1,90 +1,30 @@
 jQuery(document).ready(function(){
   "use script";
   var cardItem = $(".post__colum");
-  cardItem.mousemove(function(e){
-    // положение элемента
+  cardItem.mousemove(rotate);
+    function rotate(event){
+    //position lelement in page
     var pos = $(this).offset();
     var elem_left = pos.left;
     var elem_top = pos.top;
-    // положение курсора внутри элемента
-    var Xinner = e.pageX - elem_left;
-    var Yinner = e.pageY - elem_top;
-    
-    console.log("X: " + Xinner + " Y: " + Yinner); // вывод результата в консоль
-    
+    // position cursor in element
+    var Xinner = event.pageX - elem_left;
+    var Yinner = event.pageY - elem_top;
+    // find half width & higth element
     var halfHeight = cardItem.innerHeight() / 2;
-    
-
+    var halfWidht = cardItem.innerWidth() / 2;
+    //add css style to element
     $(this).css({
-      "transform": 'rotateX('+ -(Yinner - halfHeight) / 10 + 'deg)'
+      "transform": 'rotateX('+ -(Yinner - halfHeight) / 20 + 'deg) rotateY('+ -(Xinner - halfWidht) / 20 + 'deg)'
      });
-
-  });
-
-  cardItem.mouseleave(function(event){
+    }
+   //remove css style
+  cardItem.mouseleave(delRotate);
+    function delRotate(event){
     $(this).css({
       "transform":"rotate(0deg)",
     });
-  });
+  }
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-var 
-$card = $('.post__colum'),
-$cardTitle = $('.card-title'),
-$cardSubtitle = $('.card-subtitle'),
-$cardShine = $('.card-shine'),
-$cardShadow = $('.card-shadow'),
-currentMousePos = { x: 0, y: 0 },
-mouseFromCenter = { x: 0, y: 0 };
- 
-$(document).mousemove(function(event) {
-	var
-	wHeight= $(window).height(),
-	wWidth= $(window).width();
- 
-	currentMousePos.x = event.pageX;
-	currentMousePos.y = event.pageY;
-	mouseFromCenter.x = currentMousePos.x - (wWidth / 2);
-	mouseFromCenter.y = currentMousePos.y - (wHeight / 2);
-	
-	var 
-	around1 = -1 * (currentMousePos.y * 100 / wHeight * 0.2 - 10) + 'deg',
-	around2 = 1 * (currentMousePos.x * 100 / wWidth * 0.2 - 10) + 'deg',
-	trans1 = (currentMousePos.x * 100 / wHeight * 0.3 ) + 'px',
-	trans2 = (currentMousePos.y * 100 / wHeight * 0.3 ) + 'px',
-	dy = event.pageY - wHeight / 2, //@h/2 = center of poster
-	dx = event.pageX - wWidth / 2, //@w/2 = center of poster
-	theta = Math.atan2(dy, dx), // угол между курсором и центром картинки в RAD
-	angle = theta * 180 / Math.PI - 90,  
-	mousePositionX = ( currentMousePos.x / wWidth) * 100,
-	mousePositionY = 50+( currentMousePos.y / wHeight)*10;
- 
-	// угол градиента и "непрозрачность" для эффекта блеска карточки
-	$cardShine.css('background', 'linear-gradient(' + angle + 'deg, rgba(255,255,255,' + (currentMousePos.y / wHeight) * .7 + ') 0%,rgba(255,255,255, 0) 80%)');
-	// card pos and angle
-	$card.css({
-	"-webkit-transform": "translate3d(" + trans1 + ", " + trans2 +", 0) scale(1) rotatex(" + around1 + ") rotatey(" + around2 + ")",'background-position': mousePositionX + '%' + ' ' + (currentMousePos.y / wHeight) * 50  + '%'
-	});
-	// card shadow pos and angle
-	$cardShadow.css({"transform": "scale(.9,.9) translateX(" + ((mouseFromCenter.x * -0.02) + 12) + "px) translateY(" + ((mouseFromCenter.y * -0.02) + 12 ) + "px) scale(1.0) rotateY(" + (mouseFromCenter.x / 25) * 0.5 + "deg) rotateX(" + ((mouseFromCenter.y / -25) ) + "deg)" });
- 
-	$cardTitle.css({"transform": "translateX(" + ((mouseFromCenter.x / 25) * 0.7) + "px) translateY(" + ((mouseFromCenter.y / 25) * 1.65) + "px)"
-	});
- 
-	$cardSubtitle.css({"transform": "translateX(" + ((mouseFromCenter.x / 25) * 0.5) + "px) translateY(" + ((mouseFromCenter.y / 25) * 1.15) + "px) translateZ(60px)"
-	});
-});*/
