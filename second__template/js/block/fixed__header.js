eaa6e71a51d0abf96a7277fccaddc9e9f24c5d94
RomaVintonyak <19roman23@gmail.com>
2020-03-24 16:43:47 +0200

    fixed bags & finished section intro

--- second__template/js/block/fixed__header.js
+++ second__template/js/block/fixed__header.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function(){
   "use script";
-  var introH = $("#intro").innerHeight();
+  var header_topH = $(".header__top").innerHeight();
   var fixMenu = $(".header__bottom");
   $(window).on("scroll", function(){
-    if($(this).scrollTop() >= introH){
+    if($(this).scrollTop() >= header_topH){
       fixMenu.addClass("header__bottom--fixed");
     }else{
       fixMenu.removeClass("header__bottom--fixed");
