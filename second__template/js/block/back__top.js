7ff90d384533aab66860b2612a88627ff15fa08c
roman <19roman23@gmail.com>
2020-03-25 17:48:48 +0200

    add style to subscribe block

--- second__template/js/block/back__top.js
+++ second__template/js/block/back__top.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function(){
     "use script";
     var topBtn = $("#topBtn");
-    var pageH =  $(document).innerHeight() / 3;
+    var pageH =  $(document).innerHeight() / 4;
     topBtn.hide(1000);
     $(window).on("scroll", function(){
       if($(this).scrollTop() > pageH){
