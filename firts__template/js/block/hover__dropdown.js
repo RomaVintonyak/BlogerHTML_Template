0c470d1eb4d8d7463ffc8c3e27c9f7c46e457395
roman <19roman23@gmail.com>
2020-03-27 15:53:50 +0200

    create drop down menu to first template & write new script drop down

--- firts__template/js/block/hover__dropdown.js
+++ firts__template/js/block/hover__dropdown.js
@@ -1,19 +1,18 @@
-jQuery(document).ready(function(){
-    "use script";
-    var dropBTn = $("#dropDownItem").find(".nav__link");
-    console.log(dropBTn);
-    var dropList = $(".dpor__down");
-    dropList.hover(function(){
-      dropBTn.css({
-        "background-color": "#2B2320",
-        "color": "#ffffff"
-      });
-      dropList.mouseleave(stopHover);
-      function stopHover(){
-        dropBTn.css({
-          "background-color": "transparent",
-          "color": "#2B2320"
-        });
-      }
+jQuery(document).ready(function () {
+  "use script";
+  var dropList = $(".dpor__down");
+  dropList.hover(startHover);
+  function startHover() {
+    $(this).parent(".dropDownItem").find(".nav__link").css({
+      "background-color": "#2B2320",
+      "color": "#ffffff"
     });
+  }
+  dropList.mouseleave(stopHover);
+  function stopHover() {
+    $(this).parent(".dropDownItem").find(".nav__link").css({
+      "background-color": "transparent",
+      "color": "#2B2320"
+    });
+  }
 });
