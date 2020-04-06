b50061b7387a10f74004114aed370b534e6a9aa8
RomaVintonyak <19roman23@gmail.com>
2020-04-06 17:29:59 +0300

    add script filter cat

---
+++ firts__template/js/block/filter__cat.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function(){
+  "use script";
+  var catBtn = $("[data-link]");
+  catBtn.on("click", function(event) {
+    event.preventDefault();
+    /*add clas active for links*/
+    /*$("[data-filter]")
+      .click(function(event) {
+        event.preventDefault();
+        $("[data-filter]")
+          .removeClass("link__itmem--active")
+          .eq($(this).index())
+          .addClass("link__itmem--active");
+      })
+      .eq($(this))
+      .addClass("link__itmem--active");*/
+    /*filter menu block*/
+    var cat = $(this).data("link");
+    if (cat == "Photoshoot") {
+      $(this).removeClass("hide");
+    } else {
+      $("[data-cat]").each(function() {
+        var workCat = $(this).data("cat");
+        if (workCat != cat) {
+          $(this).addClass("hide");
+        } else {
+          $(this).removeClass("hide");
+        }
+      });
+    }
+  });
+});
