8d5017de85ad2090dbadb661e46ca2821a02a7de
roman <19roman23@gmail.com>
2020-04-13 16:30:10 +0300

    create new pag second blog template

--- second__template/js/block/burgerBtn.js
+++ second__template/js/block/burgerBtn.js
@@ -1,9 +1,11 @@
 jQuery(document).ready(function() {
   "use script";
+  var menuContent = $(".mobile__content");
   var burgerBtn = $("#burgerToggle");
   burgerBtn.on("click", function(event) {
     event.preventDefault();
     $(this).find("span").toggleClass("open");
+    menuContent.toggleClass("mobile__content--open");
   });
 });
   
