6663a1e24a0c4537d1e0f012bd36dac1d9e7b508
roman <19roman23@gmail.com>
2020-04-13 12:17:09 +0300

    create blog filter cat script

--- firts__template/js/block/filter__cat.js
+++ firts__template/js/block/filter__cat.js
@@ -20,5 +20,30 @@ jQuery(document).ready(function () {
       }
     });
   });
+  /*filter cat to blog pages */
+  var filterBtn = $("[data-filter]");
+
+  filterBtn.on("click", function (event) {
+    event.preventDefault();
+    filterBtn.not("active").removeClass("post__cat--link");
+    $(this).toggleClass("post__cat--link");
+  });
+
+  filterBtn.on("click", function (event) {
+    event.preventDefault();
+    var catB = $(this).data("filter");
+    if(catB == "all"){
+      $("[data-blog]").removeClass("post__colum--hide");
+    }else{
+    $("[data-blog]").each(function () {
+      var workCatB = $(this).data("blog");
+      if (workCatB != catB) {
+        $(this).addClass("post__colum--hide");
+      } else {
+        $(this).removeClass("post__colum--hide");
+      }
+    });
+  }
+  });
 });
 
