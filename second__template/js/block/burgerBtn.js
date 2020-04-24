1e6e7a928e0e0aa5d41098dd89ad671fe079010b
roman <19roman23@gmail.com>
2020-04-24 11:22:22 +0300

    add new burger btn to mobile menu & new burger script

--- second__template/js/block/burgerBtn.js
+++ second__template/js/block/burgerBtn.js
@@ -2,10 +2,20 @@ jQuery(document).ready(function() {
   "use script";
   var menuContent = $(".mobile__content");
   var burgerBtn = $("#burgerToggle");
+  var menuBtn = $("#mobileToggle");
+  /*open mobile menu*/
   burgerBtn.on("click", function(event) {
     event.preventDefault();
-    $(this).find("span").toggleClass("open");
-    menuContent.toggleClass("mobile__content--open");
+    $(this).find("span").addClass("open");
+    menuBtn.find("span").addClass("open");
+    menuContent.addClass("mobile__content--open");
+  });
+  /*close mobile menu*/
+  menuBtn.on("click", function(event){
+    event.preventDefault();
+    $(this).find("span").removeClass("open");
+    burgerBtn.find("span").removeClass("open");
+    menuContent.removeClass("mobile__content--open");
   });
 });
   
