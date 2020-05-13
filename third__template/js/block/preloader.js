410fe9b54d1e66c2800a768651bc129fb9ed3455
RomaVintonyak <19roman23@gmail.com>
2020-05-13 14:22:45 +0300

    add preloader third template

---
+++ third__template/js/block/preloader.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function(){
+  "use script";
+  var loader = $("#preloader");
+  var timeDelay = 1000;
+  $(window).on("load", function(){
+    setTimeout(function(){
+      loader.addClass("preloader--done");
+      $("body").css({
+        "overflow-y":"scroll"
+      });
+    }, timeDelay);
+  });
+});
